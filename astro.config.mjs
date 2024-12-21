bce6d8e969eb045f6cce4b161fb4e48627ebf78a
lakshyaraj2006 <lakshyarajdash25@gmail.com>
2024-12-21 18:24:40 +0530

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel';
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  integrations: [tailwind()],
+
+  markdown: {
+    shikiConfig: {
+      theme: "andromeeda"
+    }
+  },
+
+  devToolbar: {
+    enabled: false
+  },
+
+  adapter: vercel()
+});
