7ebf27efbd6d317f97785ec2a1bffcff4508f42e
lakshyaraj2006 <lakshyarajdash25@gmail.com>
2024-12-24 07:38:46 +0530

    Add Skills Section to home page

---
+++ src/constants.ts
@@ -0,0 +1,30 @@
+const SKILLS: string[] = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'Python',
+    'React',
+    'NodeJS',
+    'ExpressJS',
+    'Flask',
+    'Django',
+    'PHP',
+    'MongoDB',
+    'MySQL',
+];
+const SKILL_ICONS: string[] = [
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original-wordmark.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/django/django-plain.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg',
+    'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg',
+];
+
+export { SKILLS, SKILL_ICONS };
